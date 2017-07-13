05a21f0293a3db104f75dfe5127e6e19035b6ca3
schqiushui <orochi9999@gmail.com>
2017-07-13 22:11:23 +0800

    clean up
    Signed-off-by: schqiushui <orochi9999@gmail.com>

--- sm.mk
+++ sm.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 The Android Open Source Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,14 +12,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Sample: This is where we'd set a backup provider if we had one
-# $(call inherit-product, device/sample/products/backup_overlay.mk)
 
-# Inherit from the common Open Source product configuration
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, device/htc/ocn/device-ocn.mk)
 $(call inherit-product-if-exists, vendor/htc/ocn/device-vendor-ocn.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/sm/config/common_full_phone.mk)
+
+# Time Zone data for Recovery
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+PRODUCT_NAME := sm_ocn
+PRODUCT_DEVICE := ocn
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := HTC U11
+PRODUCT_MANUFACTURER := HTC
+
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+PRODUCT_GMS_CLIENTID_BASE := android-htc
 
